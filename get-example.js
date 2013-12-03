60be2454e8ab7ea6d63215b4ff39488d88a6d1fe
AJ ONeal <coolaj86@gmail.com>
2013-12-03 01:34:44 -0700

    don't overwrite the questionnaire if it exists

--- get-example.js
+++ get-example.js
@@ -1,11 +1,16 @@
-/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
 (function () {
   "use strict";
 
   var http = require('http')
     , path = require('path')
     , fs = require('fs')
+    , filename = 'questionnaire.json'
+    , filepath = path.join(__dirname, filename)
     ;
+
+  if (fs.existsSync(filepath)) {
+    return;
+  }
 
   http.get('http://pastebin.com/raw.php?i=ra0qZDe2', function (res) {
     var text = ''
@@ -15,7 +20,7 @@
       text += buf.toString('utf8');
     });
     res.on('end', function () {
-      fs.writeFileSync(path.join(__dirname, 'questionnaire.json'), text, 'utf8');
+      fs.writeFileSync(filepath, text, 'utf8');
     });
   });
 

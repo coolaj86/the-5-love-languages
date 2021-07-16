fde6d4d3de7f39c3dcb03ec3114d80de662dfd01
AJ ONeal <coolaj86@gmail.com>
2021-07-16 03:23:46 -0600

    bugfix: use https

--- get-example.js
+++ get-example.js
@@ -1,20 +1,20 @@
 "use strict";
 
-var http = require("http");
+var https = require("https");
 var path = require("path");
 var fs = require("fs");
 var filename = "questionnaire.json";
 var filepath = path.join(__dirname, filename);
-var url = "http://pastebin.com/raw/ra0qZDe2";
+var url = "https://pastebin.com/raw/ra0qZDe2";
 //var url = 'https://raw.githubusercontent.com/coolaj86/5-love-languages-test/5f4adf3e96197c8fa31e368ef24362deab222739/questionnaire.json';
 
 if (fs.existsSync(filepath)) {
   return;
 }
 
-http.get(url, function (res) {
+https.get(url, function (res) {
   if (res.statusCode < 200 || res.statusCode >= 300) {
-    console.error("failed to http.get example questionnaire");
+    console.error("failed to https.get example questionnaire");
   }
 
   var text = "";
